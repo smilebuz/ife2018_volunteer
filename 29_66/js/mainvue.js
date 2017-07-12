264f1a123e425590153fc000e7e63ba37290792c
smilebuz <smile_buz@163.com>
2017-07-12 09:33:20 +0800

    初步运用sass进行样式编写

--- 29_66/js/mainvue.js
+++ 29_66/js/mainvue.js
@@ -1,3 +1,32 @@
+Vue.component("product-item", {
+    props: ["product"],
+    template: "<li>{{ product.name }}</li>"
+});
+Vue.component("support-item", {
+    props: ["support"],
+    template: "<li>{{ support.name }}</li>"
+});
+Vue.component("about-item", {
+    props: ["about"],
+    template: "<li>{{ about.name }}</li>"
+});
+
 var vmNav = new Vue({
-    
-})
+    el: "#nav",
+    data: {
+        products: [
+            {name: "PRODUCT1"},
+            {name: "PRODUCT2"},
+            {name: "PRODUCT3"}
+        ],
+        supports: [
+            {name: "SUPPORT1"},
+            {name: "SUPPORT2"},
+            {name: "SUPPORT3"}
+        ],
+        abouts: [
+            {name: "INFO"},
+            {name: "CONTACT"}
+        ]
+    }
+});
